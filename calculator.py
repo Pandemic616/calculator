33dc84b6ed3cb205e3884c5934aa7cfccb84cdc9
Pandemic616 <graviton5000@gmail.com>
2022-05-01 19:46:09 +0500

    'v1.0'

--- calculator.py
+++ calculator.py
@@ -22,5 +22,45 @@ class Calculator:
         return self.arg1 // self.arg2
 
     def degree(self) -> int:
-        """Врзводит в степень первый аргумент на второй аргумент"""
+        """Возводит в степень первый аргумент на второй аргумент"""
         return self.arg1 ** self.arg2
+
+
+def operand(expression: str) -> str:
+    """Определяет какой операнд вырожения ввел пользователь и выводит его из строки"""
+    if '+' in expression:
+        return '+'
+    elif '-' in expression:
+        return '-'
+    elif '*' in expression:
+        return '*'
+    elif '/' in expression:
+        return '/'
+    elif '^' in expression:
+        return '^'
+
+
+def operator(operand: str, calculator: Calculator) -> int | str:
+    """Определяет какой метод класса нужно использовать по операнду"""
+    if operand in '+':
+        return calculator.plus()
+    elif operand in '-':
+        return calculator.minus()
+    elif operand in '*':
+        return calculator.multiplication()
+    elif operand in '/':
+        return calculator.divide()
+    elif operand in '^':
+        return calculator.degree()
+    else:
+        return 'Такого оператора нет'
+
+
+def main():
+    expression = input()
+    string = expression.partition(operand(expression))
+    calculator = Calculator(arg1=int(string[0]), arg2=int(string[2]))
+    print(operator(operand=string[1], calculator=calculator))
+
+
+main()
